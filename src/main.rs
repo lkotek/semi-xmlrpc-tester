1c41e1baf38ff87661b3f5f4b9e3b6966ba9a3f1
Lukas Kotek <lkotek@suse.com>
2021-03-22 17:07:55 +0100

    typos corrected

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ extern crate dotenv_codegen;
 use xmlrpc::{Request, Value};
 
 fn call_server(xmlrpc_method: &str, key: Option<&str>) -> Value {
-	let request = match key{
-		None		=> Request::new(xmlrpc_method).arg(dotenv!("MANAGER_USER")).arg(dotenv!("MANAGER_PASS")),
-		Some(val)	=> Request::new(xmlrpc_method).arg(val),
-	};
-	let result = request.call_url(dotenv!("MANAGER_URL"));
+    let request = match key {
+        None => Request::new(xmlrpc_method)
+            .arg(dotenv!("MANAGER_USER"))
+            .arg(dotenv!("MANAGER_PASS")),
+        Some(val) => Request::new(xmlrpc_method).arg(val),
+    };
+    let result = request.call_url(dotenv!("MANAGER_URL"));
     return result.unwrap();
 }
 
@@ -18,10 +20,10 @@ fn main() {
     let key = call_server("auth.login", None);
     println!("{:?}", key);
 
-	let users_list = call_server("user.list_users", key.as_str());
-	println!("{:?}", users_list);
-    
+    let users_list = call_server("user.list_users", key.as_str());
+    println!("{:?}", users_list);
+
     for user in users_list.as_array().unwrap() {
-    	println!("{:?}", user);
+        println!("{:?}", user);
     }
 }
