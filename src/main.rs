5a9da2ac664a1e85be185022c8e37d1ba8edea42
Lukas Kotek <lkotek@suse.com>
2021-03-23 17:37:43 +0100

    creation of kiwi image profiles

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,44 @@ mod scenarios;
 mod support;
 
 use std::env;
+use std::process;
 use xmlrpc::{Request, Value};
+
+fn full_deploy() {
+    println!("Sorry to say that, but there is nothing I can do right now.");
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    env::set_var("UPROFILE", &args[1]);    
+    println!("{:?}", args);
+    env::set_var("UYUNI_PROFILE", &args[1]);    
+
+    if args.len() != 3 {
+        println!("Incorrect number of arguments passed.");
+        process::exit(1);
+    }
+    match args[2].as_str() {
+        "full"  => full_deploy(),
+        _       => {
+            println!("Incorrect argument string passed.");
+            process::exit(1);
+        }
+    }
+
     let key = support::call_server("auth.login", None);
     println!("{:?}", key);
+    env::set_var("UYUNI_KEY", key.as_str().unwrap());
 
-    let users_list = support::call_server("user.list_users", key.as_str());
+    println!("SYSTEM_ID: {:?}", support::get_system_id("suma-bv-41-build-sles15sp2.mgr.prv.suse.net"));
+
+    /*
+    let users_list = support::call_server("user.list_users", Some(support::read_env("UYUNI_KEY")));
     println!("{:?}", users_list);
 
     for user in users_list.as_array().unwrap() {
         println!("{:?}", user);
-    }
+    }*/
 
-    println!("{:?}", support::import_json_data());
+    //println!("{:?}", support::import_json_data("config"));
+    println!("{:?}", support::create_kiwi_profile());
 }
--- src/support.rs
+++ src/support.rs
@@ -7,36 +7,63 @@ use std::env;
 use std::path::Path;
 use std::fs::File;
 use std::io::prelude::*;
+use json::JsonValue;
 use xmlrpc::{Request, Value};
+use std::collections::HashMap;
+
+pub fn call_server(xmlrpc_method: &str, key: Option<String>) -> Value {
+    let request = match key {
+        None => Request::new(xmlrpc_method)
+            .arg(dotenv!("UYUNI_USER"))
+            .arg(dotenv!("UYUNI_PASS")),
+        Some(val) => Request::new(xmlrpc_method).arg(val),
+    };
+    let result = request.call_url(dotenv!("UYUNI_URL"));
+    return result.unwrap();
+}
 
 pub fn read_env(env_variable: &str) -> String {
     env::var(env_variable).unwrap().to_string()
 }
 
-pub fn call_server(xmlrpc_method: &str, key: Option<&str>) -> Value {
-    let request = match key {
-        None => Request::new(xmlrpc_method)
-            .arg(dotenv!("MANAGER_USER"))
-            .arg(dotenv!("MANAGER_PASS")),
-        Some(val) => Request::new(xmlrpc_method).arg(val),
-    };
-    let result = request.call_url(dotenv!("MANAGER_URL"));
-    return result.unwrap();
-}
-
-pub fn import_json_data() {
-    let format_pathfile = format!("assets/{}/config.json", read_env("UPROFILE"));
+pub fn import_json_data(json_file: &str) -> HashMap<String, String> {
+    let format_pathfile = format!("assets/{}/{}.json", read_env("UYUNI_PROFILE"), json_file);
     let pathfile = Path::new(&format_pathfile);
     println!("{:?}", pathfile.display());
     let mut file = match File::open(pathfile) {
         Err(why) => panic!("Cannot open file!"),
         Ok(file) => file,
     };
-    let mut s = String::new();
-    println!("{:?}", file.read_to_string(&mut s));
+    let mut json_data = String::new();
+    file.read_to_string(&mut json_data);
 
-    let parse = json::parse(&s).unwrap();
-    println!("{}", parse["activation_key"]);
+    let parsed = json::parse(&json_data).unwrap();    
+    let mut parsed_data = HashMap::new();
+    for (json_key, json_value) in parsed.entries() {
+        parsed_data.insert(json_key.to_string(), json_value.to_string());
+    }
+    return parsed_data;
 }
 
-pub fn get_system_id() {}
+pub fn get_system_id(system_name: &str) -> i32 {
+    let req = Request::new("system.getId")
+        .arg(read_env("UYUNI_KEY"))
+        .arg(system_name)
+        .call_url(dotenv!("UYUNI_URL"));
+    return req.unwrap()[0]["id"].as_i32().unwrap();
+}
+
+pub fn create_kiwi_profile() -> i32 {
+    let config = import_json_data("config");
+    println!("{:?}", config);
+    let req = Request::new("image.profile.create")
+        .arg(read_env("UYUNI_KEY"))
+        .arg(config["kiwi_profile"].clone())
+        .arg("kiwi")
+        .arg(config["image_store"].clone())
+        .arg(config["profile_path"].clone())    
+        .arg(config["activation_key"].clone())
+        .call_url(dotenv!("UYUNI_URL").clone());
+    return req.unwrap().as_i32().unwrap();
+}
+
