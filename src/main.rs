e7f30a139dcd50855f7f4b2e85f41779bb0f31ac
Lukas Kotek <lkotek@suse.com>
2021-05-13 15:26:07 +0200

    help added

--- src/main.rs
+++ src/main.rs
@@ -26,14 +26,51 @@ fn basic_tests() {
     ));
 }
 
+fn help() {
+    println!(
+        "Usage:
+    semi-xmlrpc-tester [-y] [--silent | --debug] [environment] [scenario]
+
+    Example:
+    semi-xmlrpc-tester -y --debug 15sp2 basic_test
+
+    Options:
+    ========
+    -y          - answer all questions of programm 'yes'
+    --silent    - run programm without any printed output
+    --debug     - set log level to DEBUG (default is INFO)
+
+    Environment:
+    ============
+    Use directory from 'assets' containing set of json config files to be used
+
+    Scenarios:
+    ==========
+    basic_tests - log to server and print users on it
+    formulas    - configure all retail formulas at branch server
+    buildhost   - set entitlement for buildhost and apply highstate
+    profiles    - prepare kiwi image profiles
+    image       - build kiwi image
+    saltboot    - configure saltboot formula
+    prepare     - create necessary groups and apply highstate at branch server
+    full        - run full deployment (all above) in proper order (as above)
+    "
+    );
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.contains(&"-h".to_string()) {
+        help();
+        process::exit(0);
+    }
     env::set_var("UYUNI_PROFILE", &args[1]);
 
     if args.len() < 3 {
         println!("Incorrect number of arguments passed. Closing.");
         process::exit(1);
     }
+
     if args.contains(&"-y".to_string()) {
         env::set_var("UYUNI_YES", "yes"); // Answer all script question as yes
     } else {
@@ -51,10 +88,10 @@ fn main() {
         support::read_env("UYUNI_LOG_LEVEL")
     ));
     support::import_json_data("config.json");
-    
+
     let key = support::call_server("auth.login", None);
     env::set_var("UYUNI_KEY", key.as_str().unwrap());
-        
+
     match args[2].as_str() {
         "basic_tests" => basic_tests(),
         "formulas" => scenarios::configure_retail_formulas(),
